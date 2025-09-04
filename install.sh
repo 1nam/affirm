570b1380e0a060d4959cdb420ab3f02e796c7d30
z7eus <33592162+1nam@users.noreply.github.com>
2025-09-04 09:44:11 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ SCRIPT_NAME="affirm"
 echo "🔮 Installing $SCRIPT_NAME..."
 
 # Download the script from GitHub
-curl -s -o /tmp/$SCRIPT_NAME https://raw.githubusercontent.com/yourusername/affirm/main/affirm
+curl -s -o /tmp/$SCRIPT_NAME https://github.com/1nam/affirm/blob/main/affirm
 
 chmod +x /tmp/$SCRIPT_NAME
 sudo mv /tmp/$SCRIPT_NAME $INSTALL_DIR/
